fa08f7d34b2710480fc4209f99b72dbc735acede
Goran <144773469+Lucifer87878@users.noreply.github.com>
2024-02-14 09:26:38 +0100

    Add files via upload

---
+++ Solarsystem/script.js
@@ -0,0 +1,87 @@
+// FETCH API DATA AND CREATE LANDING PAGE PLANETS
+const apiUrl = "https://majazocom.github.io/Data/solaris.json";
+const landingPage = document.querySelector(".solarsystem");
+const planetsCont = document.querySelector(".planets__cont");
+let solarSystem = [];
+
+async function fetchSolarSystem() {
+  try {
+    let resp = await fetch(apiUrl);
+    solarSystem = await resp.json();
+
+    solarSystem.forEach((planet) => {
+      const planetElement = document.createElement("figure");
+      planetElement.id = planet.name;
+      if (planet.name === "Solen") {
+        landingPage.appendChild(planetElement);
+      } else {
+        planetsCont.appendChild(planetElement);
+      }
+
+      planetElement.addEventListener("click", () => renderPlanetInfo(planet));
+    });
+  } catch (error) {
+    console.error("Error fetching solar system data:", error);
+  } 
+}
+fetchSolarSystem();
+
+// TARGET THE RIGHT ELEMENT FOR HTML MODAL
+const modal = document.querySelector(".modal");
+const planetName = document.querySelector(".name");
+const latinName = document.querySelector(".name__latin");
+const about = document.querySelector(".about");
+const circumf = document.querySelector(".circumf");
+const distance = document.querySelector(".distance");
+const tempDay = document.querySelector(".temp__day");
+const tempNight = document.querySelector(".temp__night");
+const moonUl = document.querySelector(".moons");
+
+// RENDER INFO IN THE RIGHT PLACE AND HIDE/SHOW ELEMENTS ON CLICK
+function renderPlanetInfo(planet) {
+  modal.classList.remove("hidden");
+  landingPage.classList.add("hidden");
+
+  planetName.innerText = planet.name.toUpperCase();
+  latinName.innerText = planet.latinName.toUpperCase();
+  about.innerText = planet.desc;
+  circumf.innerText = `${planet.circumference} km`;
+  distance.innerText = `${planet.distance} km`;
+  tempDay.innerText = planet.temp.day;
+  tempNight.innerText = planet.temp.night;
+
+  moonUl.innerText = "";
+  const moons = planet.moons;
+  if (moons.length === 0) {
+    const li = document.createElement("li");
+    li.innerText = "Inga månar";
+    moonUl.appendChild(li);
+  } else {
+    moons.forEach((moon) => {
+      const li = document.createElement("li");
+      li.innerText = moon;
+      moonUl.appendChild(li);
+    });
+  }
+}
+
+//CLOSE MODAL WITH CLICK ON X
+const btnClose = document.querySelector(".fa-x");
+btnClose.addEventListener("click", () => {
+  landingPage.classList.remove("hidden");
+  modal.classList.add("hidden");
+});
+
+// CREATE STARS
+const starCont = document.querySelector(".stars__cont");
+
+function createStars() {
+  for (let i = 0; i < 50; i++) {
+    const star = document.createElement("figure");
+    star.className = "star";
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    starCont.appendChild(star);
+  }
+}
+createStars();
